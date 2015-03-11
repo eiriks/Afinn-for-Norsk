60ba7b4c6d96a429de2cbdd0745640bd29968198
Eirik <eirik@stavelin.com>
2015-03-11 13:24:45 +0100

    Plukka dette opp igjen og testa
    Hvis jeg kan lage en bra verktøy for å la folk hjelpe oss å vurdere
    ords vekt fra -5 til 5, så kan dette kanskje bli bra?

--- test.py
+++ test.py
@@ -8,7 +8,7 @@
 # ==========================================================================
 # = I dette dokumentet leker jeg med afinn-ordlisten til Finn Årup Nielsen
 # = målet var kort og enkelt: fungerer dette på norsk?
-# = det jeg har gjort er lynraskt å la google oversette Finns liste til Norsk 
+# = det jeg har gjort er lynraskt å la google oversette Finns liste til Norsk
 # = (som kjent fungerer det sånn halvveis, men det er i alle fall et sted å starte, og det gikk fort)
 # =
 # = jeg har ikke testet dette på annet en små tekststrenger, og aner egentig ikke om det virker.
@@ -34,7 +34,7 @@ for line in codecs.open('./norsk.txt', 'r', 'utf-8-sig'):
     key, val = line.split('\t')
     finn[key] = int(val)
 
-# dette er en endring slik at git   ser det  
+# dette er en endring slik at git   ser det
 
 # AFINN-111 is as of June 2011 the most recent version of AFINN
 # filenameAFINN = 'AFINN/norsk.txt'
@@ -48,9 +48,9 @@ Returns a float for sentiment strength based on the input text.
 Positive values are positive valence, negative value are negative valence.
 """
     #words = pattern_split.split(text.lower())
-    
+
     # tester ulike tokenizers
-    #words2 = nltk.word_tokenize(text.decode('utf8')) #decode converterer str til unicode 
+    #words2 = nltk.word_tokenize(text.decode('utf8')) #decode converterer str til unicode
     # [u'skaml', u'\xf8', u's', u'!', u'r', u'\xf8', u'r.', u's', u'\xf8', u'l', u',', u'denne', u'burde', u'ha', u'v', u'\xe6', u'rt', u'|', u'\\', u'|', u'egat1', u'\\', u'/']
     #words3 = nltk.blankline_tokenize(text.decode('utf8'))
     # [u'skaml\xf8s! r\xf8r. s\xf8l, denne burde ha v\xe6rt |\\|egat1\\/']
@@ -61,14 +61,14 @@ Positive values are positive valence, negative value are negative valence.
     # print words2
     # print words3
     # print words4
-    
+
     sentiments = map(lambda word: finn.get(word, 0), words4)
     if sentiments:
         #print sentiments
         # How should you weight the individual word sentiments?
         # You could do N, sqrt(N) or 1 for example. Here I use sqrt(N)
         sentiment = float(sum(sentiments))/math.sqrt(len(sentiments))
-        
+
     else:
         sentiment = 0
     return sentiment
@@ -87,46 +87,52 @@ if __name__ == '__main__':
     # print repr(text)
     # print repr(test2)
     # print repr(test)
-    # 
+    #
     # if test == test2:
     #     print "true"
     # else:
-    #     print "false" 
+    #     print "false"
     # print("%6.2f %s" % (sentiment(text), text))
-    
+
     text = "skamløs! rør. søl, denne burde ha vært negativ"
     print("%6.2f %s" % (sentiment(text), text))
-     
+
     text = "Finn er dum, feit, lat. og skamløs"
     print("%6.2f %s" % (sentiment(text), text))
-    
+
     text = "søt hyggelig snill glad happy sex"
     print("%6.2f %s" % (sentiment(text), text))
-    # 
+    #
     # text = "verden går til helvete, herfra går det bare ned"
     # print("%6.2f %s" % (sentiment(text), text))
-    # 
+    #
     # text = "det skader ikke å gå planken, men det kan være uheldig."
     # print("%6.2f %s" % (sentiment(text), text))
-    
+
     text = "statan i helvete dette er det kipeste jeg kan faenmag tenke på"
     print("%6.2f %s" % (sentiment(text), text))
-    
+
     text = "dette er fantastisk, jeg gleder meg over framgangen vi oppnår her"
     print("%6.2f %s" % (sentiment(text), text))
-    
+
     text = "Eirik er dritkul"
     print("%6.2f %s" % (sentiment(text), text))
-    
+
     text = "Eirik er dritkul :("
     print("%6.2f %s" % (sentiment(text), text))
-    
+
     text = "@Trinesg var streng med rette i dag. Bra! #aktuelt"
     print("%6.2f %s" % (sentiment(text), text))
-    
+
     text = "@eidem Nei. At det er frivillig, og kan kreves slettet til enhver tid, hjelper. Men målet bør være null lagring, jf målet i EU-rett før #dld"
     print("%6.2f %s" % (sentiment(text), text))
-    
+
+
+    text = """Dokumentasjonen NRK fremlegger bekrefter så vidt vi kan se at dette produktet holder seg innenfor myndighetenes grenseverdier, noe som er i samsvar med dokumentasjonen vi har på dette produktet. Vi vil fase ut dette produktet og erstatte det med et PFOA-fritt alternativ av like høy kvalitet."""
+    print("%6.2f %s" % (sentiment(text), text))
+
+    text = """Det er morsomt at de snille "humanister" nu reagerer på overvåkningstjenestenhs aktiviteter. "Humanistene" glemmer at vi har en aktiv og effektiv mafia i arbeid. Eksempelvis dreper latviske bander for stykkpris kr 80.000,-, og de slår da i hjel offeret med balltre. Mafiaen vil gjerne eksponere og bekjempe PST, og får god hjelp av snillister og spesielle advokater. Kanskje "snllistene" skal stikke fingeren i jorden og erkjenne hvordan samfunnet har blitt etter en ukontrollert import fra syd-øst? Det er f.eks. dessverre et faktum at mye kriminalitet organsieres via det "humane tiggermiljø". De nyttige idioter vil alltid bistå de destruktive krefter!"""
+    print("%6.2f %s" % (sentiment(text), text))
     # text = """
     # Klima- og forureiningsdirektoratet rår Miljøverndepartementet til å utsetja avgjera om fjordeponi  i Førdefjorden i eit år.
     # Betre utgreiing først
@@ -142,17 +148,17 @@ if __name__ == '__main__':
     # - Det var eit klart fleirtall og med dette vedtaket har vi lagt til rette for 170 nye arbeidsplassar i kommunen, sa ordfører Håkon Myrvang (Ap) til bt.no i fjor.
     # """
     # print("%6.2f %s" % (sentiment(text), text))
-    
-    
+
+
     # No negation and booster words handled in this approach
     # text = "Finn is only a tiny bit stupid and not idiotic"
     # print("%6.2f %s" % (sentiment(text), text))
-    
-    
+
+
     # Example with downloading from Twitter:
     # import simplejson
     # import urllib
-    # 
+    #
     # query = "pfizer"
     # json = simplejson.load(urllib.urlopen("http://search.twitter.com/search.json?q=" + query))
     # sentiments = map(sentiment, [ tweet['text'] for tweet in json['results'] ])
